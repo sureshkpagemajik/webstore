356753b7ae221e4705b650e6279b46cf56078b7b
NarayanSampath <narayanfox@outlook.com>
2020-09-08 11:32:54 +0530

    Update copyegbtm.sh

--- copyegbtm.sh
+++ copyegbtm.sh
@@ -1,15 +1,15 @@
-@echo off
+#!/bin/bash
 
-set pwd=%cd%
-set source="/opt/egurkha/lib/apm/Java/default"
-cp $source  "$pwd\Modules\customers\egbtm\" /s /Y
-cp $source  "$pwd\Modules\customers\egbtm\" /s /Y
-cp $source  "$pwd\Modules\customers\egbtm\" /s /Y
-cp $source  "$pwd\Modules\gateway\egbtm\" /s /Y
-cp $source  "$pwd\Modules\order\egbtm\" /s /Y
-cp $source  "$pwd\Modules\payment\egbtm\" /s /Y
-cp $source  "$pwd\Modules\products\egbtm\" /s /Y
-cp $source  "$pwd\Modules\quote\egbtm\" /s /Y
-cp $source  "$pwd\Modules\webstore\egbtm\" /s /Y
+source=/opt/egbtm/
+
+cp -R $source  $PWD/Modules/customers/egbtm/
+cp -R $source  $PWD/Modules/customers/egbtm/
+cp -R $source  $PWD/Modules/customers/egbtm/
+cp -R $source  $PWD/Modules/gateway/egbtm/
+cp -R $source  $PWD/Modules/order/egbtm/
+cp -R $source  $PWD/Modules/payment/egbtm/
+cp -R $source  $PWD/Modules/products/egbtm/
+cp -R $source  $PWD/Modules/quote/egbtm/
+cp -R $source  $PWD/Modules/webstore/egbtm/
 
 echo copied to all Microservices.
